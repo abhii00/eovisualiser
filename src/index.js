24276e017e97d2d8731665163c2e3a0d60ce4185
abhi00 <abhijit.pandit808@gmail.com>
2021-07-17 16:25:47 +0100

    template code in react app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+
+import { App } from './components/components.js'
+
+import './index.css'
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
